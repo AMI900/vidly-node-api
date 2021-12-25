77c4cabe6bcec9610948e26d2ec2e58dee499e91
AnujMaurya32 <anujmaurya563400@gmail.com>
2021-12-25 12:25:52 +0530

    Few Changes

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,8 +1,10 @@
+const config = require('config');
 module.exports = function (req, res, next) {
   // 401 Unauthorized
   // 403 Forbidden
-  console.log(req.user.isAdmin);
-  if (!req.user.isAdmin) return res.status(403).send('Access Denied');
+  if (config.get('requiresAuth')) {
+    if (!req.user.isAdmin) return res.status(403).send('Access Denied');
+  }
 
   next();
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,19 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No Token provided.');
+  if (config.get('requiresAuth')) {
+    const token = req.header('x-auth-token');
+    if (!token)
+      return res.status(401).send('Access denied. No Token provided.');
 
-  try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    req.user = decoded;
+    try {
+      const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+      req.user = decoded;
+      next();
+    } catch (ex) {
+      res.status(400).send('Invalid token');
+    }
+  } else {
     next();
-  } catch (ex) {
-    res.status(400).send('Invalid token');
   }
 };
